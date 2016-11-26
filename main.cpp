8dbc4e18cbe85c80a87277d90e0ab9b44021db55
jon <jon.programming@gmail.com>
2016-11-26 17:27:22 -0500

    chaned main

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char** argv)
     Text text(words);
     cout << "freq of " << argv[1] <<" " << text.getProportion(argv[1]) << endl;
     // test edit distance
-    string str1 = "monday";
-    string str2 = "tuesday";
+    string str1 = argv[1]; 
+    string str2 = argv[2]; 
     cout << "Edit distance is " << editDistance(str1,str2) << endl;
     return 0;
 }
